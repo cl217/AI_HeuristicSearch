89127fad3a187574514c76d4d08fac8c0a752f45
Cindy Lin <cindy.lin217@gmail.com>
2020-10-04 22:57:40 -0400

    made temporary ForTesting.java class

--- src/Main.java
+++ src/Main.java
@@ -86,19 +86,8 @@ public class Main {
 		}
 		in.close();
 		
-		testPrintMapToFile("C:\\Users\\Cindy\\Desktop\\map.txt");
+		ForTesting.testPrintMapToFile(map, "C:\\Users\\Cindy\\Desktop\\map.txt");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
@@ -346,111 +335,5 @@ public class Main {
 		
 	}
 	
-	
-	
-	/** Just using these for debugging */
-	public static void testPrintMap() {
-		System.out.println();
-		String space = "     ";
-		System.out.print(space);
-		for(int i = 0; i < nCols; i++) {
-			if( i < 10) {
-				System.out.print(" "+i+"   ");
-			}else if ( i < 100) {
-				System.out.print(" "+i+"  ");
-			}else{
-				System.out.print(" "+i+" ");
-			}
-		}
-		System.out.println();
-		System.out.print(space);
-		for(int i = 0; i < nCols; i++ ) {
-			System.out.print(" ____");
-		}
-		System.out.println();
-		
-		for(int i = 0; i < nRows; i++) {
-			if( i < 10) {
-				System.out.print("   "+i+" ");
-			}else if ( i < 100) {
-				System.out.print("  "+i+" ");
-			}else{
-				System.out.print(" "+i+" ");
-			}
-			
-			for(int k = 0; k < nCols; k++) {
-				if(map[i][k] == '1' || map[i][k] == '2') {
-					System.out.print("|    ");
-				}else {
-					System.out.print("|" + map[i][k] + "   ");	
-				}
-
-			}
-			System.out.println("|");
-			System.out.print(space);
-			for(int k = 0; k < nCols; k++) {
-				System.out.print("|____");
-			}
-			System.out.println("|");
-		}
-	}
-	public static void testPrintMapToFile(String path) {
-	    try {
-	        FileWriter writer = new FileWriter(path);
-			writer.write("\n");
-			String space = "     ";
-			writer.write(space);
-			for(int i = 0; i < nCols; i++) {
-				if( i < 10) {
-					writer.write(" "+i+"   ");
-				}else if ( i < 100) {
-					writer.write(" "+i+"  ");
-				}else{
-					writer.write(" "+i+" ");
-				}
-			}
-			writer.write("\n");
-			writer.write(space);
-			for(int i = 0; i < nCols; i++ ) {
-				writer.write(" ____");
-			}
-			writer.write("\n");
-			
-			for(int i = 0; i < nRows; i++) {
-				if( i < 10) {
-					writer.write("   "+i+" ");
-				}else if ( i < 100) {
-					writer.write("  "+i+" ");
-				}else{
-					writer.write(" "+i+" ");
-				}
-				
-				for(int k = 0; k < nCols; k++) {
-					if(map[i][k] == '1' || map[i][k] == '2') {
-						writer.write("|    ");
-					}else {
-						writer.write("|" + map[i][k] + "   ");	
-					}
-
-				}
-				writer.write("|");
-				writer.write("\n");
-				writer.write(space);
-				for(int k = 0; k < nCols; k++) {
-					writer.write("|____");
-				}
-				writer.write("|");
-				writer.write("\n");
-			}
-	        
-	        
-	        
-	        
-	        writer.close();
-	      } catch (IOException e) {
-	        System.out.println("An error occurred.");
-	        e.printStackTrace();
-	      }
-	}
 
 }
