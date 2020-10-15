e478c51828cab8a17276072475ad6ed30bd0b557
amie-design <68176157+amie-design@users.noreply.github.com>
2020-10-15 10:58:23 -0400

    started succ(s) (Bottom of page 3 of PDF)

--- src/HeuristicSearch.java
+++ src/HeuristicSearch.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 /*
@@ -77,17 +78,78 @@ public abstract class HeuristicSearch {
 		return output;
 	}
 
-	
 	/**
 	 * TODO: Algorithm to find shortest path here
 	 * @return ArrayList of Cells = path from start to end
 	 */
 	public ArrayList<Cell> search(){
 		ArrayList<Cell> path = new ArrayList<Cell>();
-		//...
+		ArrayList<Cell> visited = new ArrayList<Cell>();
+		
+		Cell start = new Cell('S', Main.start);
+		
+		path.add(start);
+		
+		Cell goal = new Cell('G', Main.goal);
+		
 		return path;
 	}
+	
+	//Max possible vertices is 8 (PDF 3)
+	public static ArrayList<Cell> succ(Cell s) {
+		
+				ArrayList<Cell> successors = new ArrayList<Cell>();
+		
+				int x1 = s.coordinate[0];
+				int y1 = s.coordinate[1];
+				
+				
+				int maxEdges = 8;
+				
+				//BROKEN <-- have to make sure Cells are not out of bounds first
+				
+				Cell n1 = new Cell(Main.map[x1 - 1][y1 + 1].c, new int[] {x1 - 1, y1+1});						
+				Cell n2 = new Cell(Main.map[x1 - 1][y1].c, new int[] {x1 - 1, y1});
+				Cell n3 = new Cell(Main.map[x1 - 1][y1 - 1].c, new int[] {x1 - 1, y1 - 1});
+				Cell n4 = new Cell(Main.map[x1][y1 + 1].c, new int[] {x1, y1 + 1});
+				Cell n5 = new Cell(Main.map[x1][y1 - 1].c, new int[] {x1, y1 - 1});
+				Cell n6 = new Cell(Main.map[x1 + 1][y1 + 1].c, new int[] {x1 + 1, y1 + 1});
+				Cell n7 = new Cell(Main.map[x1 + 1][y1].c, new int[] {x1 + 1, y1});
+				Cell n8 = new Cell(Main.map[x1 + 1][y1 - 1].c, new int[] {x1 + 1, y1 - 1});
+				
+				System.out.print("Neighbor 1: (" + n1.x() + "," + n1.y() + ")" );
+				System.out.print("Neighbor 2: (" + n2.x() + "," + n2.y() + ")" );				
+				System.out.print("Neighbor 3: (" + n3.x() + "," + n3.y() + ")" );
+				System.out.print("Neighbor 4: (" + n4.x() + "," + n4.y() + ")" );
+				System.out.print("Neighbor 5: (" + n5.x() + "," + n5.y() + ")" );
+				System.out.print("Neighbor 6: (" + n6.x() + "," + n6.y() + ")" );
+				System.out.print("Neighbor 7: (" + n7.x() + "," + n7.y() + ")" );
+				System.out.print("Neighbor 8: (" + n8.x() + "," + n8.y() + ")" );
+				
+				successors.add(n1);
+				successors.add(n2);
+				successors.add(n3);
+				successors.add(n4);
+				successors.add(n5);
+				successors.add(n6);
+				successors.add(n7);
+				successors.add(n8);
+				
+				for(int i = 0; i < maxEdges; i++) {
+					
+					if(successors.get(i).c == 'b' || successors.get(i).x() == -1  || successors.get(i).y() == -1
+							|| successors.get(i).x() == 50 || successors.get(i).y() == 30) {
+						
+						successors.remove(i);
+					}
+					
+					
+				}
+				
+				
+				return successors;
+	}
 
-
-
+//|| n[i][1] == -1 || n[i][0] == 50 || n[i][1] == 30
 }
+	
